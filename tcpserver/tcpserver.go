aa2571c070b525d186e1d522f24ada0a058844ef
Ty Christensen <tychristensen97@gmail.com>
2022-01-15 16:20:28 -0500

    added functional tcp server

--- tcpserver/tcpserver.go
+++ tcpserver/tcpserver.go
@@ -1,13 +1,17 @@
 package tcpserver
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
 )
 
-func StartServer(readyChan chan bool, msgChan chan string, address string) {
-	<-readyChan
+// StartServer starts a simple server process that listens for TCP connections
+// on port localhost:9500 and returns a funny resonse on the connection
+func StartServer(readyChan chan bool) {
+	// <-readyChan
+	println("server started")
 	listener, err := net.Listen("tcp", ":9500")
 	if err != nil {
 		panic(fmt.Errorf("failed to make listener: %v", err))
@@ -21,7 +25,29 @@ func StartServer(readyChan chan bool, msgChan chan string, address string) {
 		}
 
 		log.Println("Accepted", conn.RemoteAddr())
-		conn.Write([]byte("damn boi\n"))
-		conn.Close()
+		conn.Write([]byte(">"))
+		go handleConn(conn)
+	}
+}
+
+// handleConn handles the new connection accepted in StartServer
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	s := bufio.NewScanner(conn)
+
+	for s.Scan() {
+		data := s.Text()
+
+		if data == "" {
+			conn.Write([]byte(">"))
+			continue
+		}
+
+		if data == "exit" {
+			return
+		}
+
+		conn.Write([]byte("damn boi\n>"))
 	}
 }
