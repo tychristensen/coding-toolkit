ca3638b714ba1dafee1df033cb8abe039d2f0afd
Ty Christensen <tychristensen97@gmail.com>
2022-03-27 10:36:38 -0400

    add books package

---
+++ book/book.go
@@ -0,0 +1,17 @@
+package book
+
+import "github.com/fxtlabs/date"
+
+// Book is a data structure that represents a book
+type Book struct {
+	Author  string
+	PubDate date.Date
+}
+
+// ByPubDate implements sort.Interface for []Book based
+// on the PubDate field in ascending (oldest first) order
+type ByPubDate []Book
+
+func (p ByPubDate) Len() int           { return len(p) }
+func (p ByPubDate) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p ByPubDate) Less(i, j int) bool { return p[i].PubDate.Before(p[j].PubDate) }
--- go.mod
+++ go.mod
@@ -5,12 +5,14 @@ go 1.18
 require (
 	github.com/fxtlabs/date v0.0.0-20150819233934-d9ab6e2a88a9
 	github.com/onsi/ginkgo/v2 v2.1.3
-	github.com/onsi/gomega v1.18.1
+	github.com/onsi/gomega v1.19.0
 )
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
